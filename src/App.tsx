1f70ec5debe4ef9a29a9429acde333aeb46a1c8d
miguel <mandresmoquera@gmail.com>
2023-06-14 11:32:54 -0600

    finishing toogle btn, starting home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import Home from "@/scenes/home";
+import { useState, useEffect } from "react";
 
 import { SelectedPage } from "@/shared/types";
 
+// TODO 1:44:50
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
